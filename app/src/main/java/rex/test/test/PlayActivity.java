f5ae6795c83b2cb03fd9b69708b052202f9d7a77
Jatin Kumar <jatin.kumarroxx@hotmail.com>
2018-02-20 09:53:51 -0500

    Image in place white box in second screen

--- app/src/main/java/rex/test/test/PlayActivity.java
+++ app/src/main/java/rex/test/test/PlayActivity.java
@@ -1,7 +1,9 @@
 package rex.test.test;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -22,13 +24,14 @@ public class PlayActivity extends AppCompatActivity {
     private Circle circle;
     private ImageView gameFrame;
     private Rectangle rectangle;
+    private Resources gameResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-
+        gameResources = getResources();
         gameFrame =(ImageView) findViewById(R.id.CanvasImageView);
         WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
@@ -50,9 +53,23 @@ public class PlayActivity extends AppCompatActivity {
         gameFrame.setImageBitmap(gameBitmap);
         //Values for Xs and Ys
         rectangle = new Rectangle(new Point(250,250), new Point(500,400));
-        drawRectangle(rectangle,"DRAW");
+        //drawRectangle(rectangle,"DRAW");
+        drawBitmap(rectangle,"DRAW");
 
     }
+
+    private void drawBitmap(Rectangle rectangle, String action)
+    {
+        if (action.equals("DRAW"))
+            paint.setColor(Color.WHITE);
+        else
+            paint.setColor(Color.BLACK);
+        //Get Function for x's and y's for rectangle
+        Rect temp= new Rect(rectangle.getM_TopLeft().getM_X(),rectangle.getM_TopLeft().getM_Y(), rectangle.getM_BottomRight().getM_X(),rectangle.getM_BottomRight().getM_Y());
+        Bitmap myBitmap = BitmapFactory.decodeResource(gameResources,R.drawable.strawberry);
+        gameCanvas.drawBitmap(myBitmap,0,0,paint);
+    }
+
     //Draw function for rectangle
     private void drawRectangle(Rectangle rectangle, String action)
     {
